7f0e323ed4427bb1819031c1a725e24e140190b0
Zamiell <5511220+Zamiell@users.noreply.github.com>
2025-09-13 08:21:34 -0400

    fix: lint

--- knip.config.js
+++ knip.config.js
@@ -8,6 +8,9 @@ const config = {
   ignore: [
     "eslint.config.mjs", // ESLint is provided by "complete-lint".
     "prettier.config.mjs", // Prettier is provided by "complete-lint".
+
+    // https://github.com/webpro-nl/knip/issues/1261
+    ".github/workflows/setup/action.yml",
   ],
   ignoreDependencies: [
     "complete-lint", // This is a linting meta-package.
